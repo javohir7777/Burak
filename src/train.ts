a35c4822afaec43e556d03bf1a317956badc9f0d
javohir7777 <javohirjumayev7777@gmail.com>
2026-08-29 06:51:30 +0500

    feat: create project standards

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+/** Project Standards
+      -Login standards
+      -Naming standards
+        function, method, variables => CAMEL
+        class => PASCAL
+        folder => CABAB
+        css => SNAKE
+      -Eror handling
+ */
