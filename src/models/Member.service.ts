2ee9efebf126dfab4478fcbc1d516db6907e85e0
javohir7777 <javohirjumayev7777@gmail.com>
2026-08-30 11:26:18 +0500

    feat: develop signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,5 +1,28 @@
+import MemberModel from "../schema/Member.model";
+import { Member, MemberInput } from "../libs/types/member";
+import Errors, { HttpCode, Message } from "../libs/Errors";
+import { MemberType } from "../libs/enums/member.enum";
+
 class MemberService {
-  constructor() {}
+  private readonly memberModel;
+
+  constructor() {
+    this.memberModel = MemberModel;
+  }
+  public async processSignup(input: MemberInput): Promise<Member> {
+    const exist = await this.memberModel
+      .findOne({ memberType: MemberType.RESTAURNT })
+      .exec();
+    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATED_FAILED);
+
+    try {
+      const resualt = await this.memberModel.create(input);
+      resualt.memberPassword = "";
+      return resualt;
+    } catch (err) {
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATED_FAILED);
+    }
+  }
 }
 
 export default MemberService;
