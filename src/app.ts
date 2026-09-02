dcbdefdf2d9f2f150c6e188d3e8e03ee8b976c2c
javohir7777 <javohirjumayev7777@gmail.com>
2026-09-02 06:28:22 +0500

    feat: frontend related css, js and image creation

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(morgan(MORGAN_FORMAT));
 
 // 3-VIEWS
 app.set("views", path.join(__dirname, "views"));
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 
 // 4-ROUTERS
 app.use("/admin", routerAdmin); // SSR
--- src/controllers/restaurant.controllers.ts
+++ src/controllers/restaurant.controllers.ts
@@ -10,9 +10,7 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send("Home Page");
-
-    // send | json | redirect | end | render
+    res.render("home");
   } catch (err) {
     console.log("Error, goHome", err);
   }
@@ -22,7 +20,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
 
-    res.send("Signup Page");
+    res.render("signup");
   } catch (err) {
     console.log("Error, getSignup", err);
   }
@@ -32,7 +30,7 @@ restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
 
-    res.send("Login Page");
+    res.render("login");
   } catch (err) {
     console.log("Error, getLogin", err);
   }
