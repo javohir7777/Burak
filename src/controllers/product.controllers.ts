dd59696471514ec16f63abf59631a9c688dfaa0c
javohir7777 <javohirjumayev7777@gmail.com>
2026-09-10 05:54:00 +0500

    feat: create new product frontend logic develop

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -43,14 +43,14 @@ productController.createNewProduct = async (
 
     await productService.createNewProduct(data);
     res.send(
-      `<script>alert("Sucessfull creation!"); window.location.replace("admin/product/all")</script>`,
+      `<script>alert("Sucessfull creation!"); window.location.replace("/admin/product/all")</script>`,
     );
   } catch (err) {
     console.log("Error, createNewProduct ", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script>alert("${message}"); window.location.replace("admin/product/all")</script>`,
+      `<script>alert("${message}"); window.location.replace("/admin/product/all")</script>`,
     );
   }
 };
