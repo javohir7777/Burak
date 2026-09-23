c7e53e02b8343f850a2da69a6b4ecf6a6be50a86
javohir7777 <javohirjumayev7777@gmail.com>
2026-09-23 10:48:45 +0500

    feat: create middleware methods and develop logout process

--- src/controllers/member.controllers.ts
+++ src/controllers/member.controllers.ts
@@ -1,8 +1,13 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 import { T } from "../libs/types/comman";
 import MemberService from "../models/Member.service";
-import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import {
+  ExtendedRequest,
+  LoginInput,
+  Member,
+  MemberInput,
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
@@ -44,25 +49,56 @@ memberController.login = async (req: Request, res: Response) => {
 
     res.status(HttpCode.OK).json({ member: result, accessToken: token });
   } catch (err) {
-    console.log("Error, signup ", err);
+    console.log("Error, login ", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
 
-memberController.verifyAuth = async (req: Request, res: Response) => {
+memberController.logout = async (req: ExtendedRequest, res: Response) => {
   try {
-    let member = null;
-    const token = req.cookies["accessToken"];
-    if (token) member = await authService.checkAuth(token);
-    if (!member)
-      throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
-
-    res.status(HttpCode.OK).json({ member: member });
+    console.log("logout");
+    res.cookie("accessToken", null, { maxAge: 0, httpOnly: false });
+    res.status(HttpCode.OK).json({ logout: true });
   } catch (err) {
-    console.log("Error, verifyAuth ", err);
+    console.log("Error, logout", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.verifyAuth = async (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies["accessToken"];
+    if (token) req.member = await authService.checkAuth(token);
+    if (!req.member)
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
+
+    next();
+  } catch (err) {
+    console.log("Error, verifyAuth", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.retrieveAuth = async (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies["accessToken"];
+    if (token) req.member = await authService.checkAuth(token);
+
+    next();
+  } catch (err) {
+    console.log("Error, retrieveAuth ", err);
+    next();
   }
 };
 
