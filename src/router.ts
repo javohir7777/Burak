69c026be85f875008c923cc768089ef61060711f
javohir7777 <javohirjumayev7777@gmail.com>
2026-09-23 22:44:48 +0500

    feat: develop updateMember business logic

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import memberController from "./controllers/member.controllers";
 const router = express.Router();
+import uploader from "./libs/utils/uploader";
 
 /** Member **/
 router.post("/member/login", memberController.login);
@@ -14,6 +15,13 @@ router.get(
   "/member/detail",
   memberController.verifyAuth,
   memberController.getMemberDetail,
+);
+
+router.post(
+  "/member/update",
+  memberController.verifyAuth,
+  uploader("members").single("memberImage"),
+  memberController.updateMember,
 );
 
 /** Product **/
