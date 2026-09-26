bad5edc1fbe0322a56ea5cbda324ce12cfbf9124
javohir7777 <javohirjumayev7777@gmail.com>
2026-09-26 12:34:55 +0500

    fix: modify getProduct business logic

--- src/models/View.service.ts
+++ src/models/View.service.ts
@@ -1,9 +1,26 @@
+import Errors, { HttpCode, Message } from "../libs/Errors";
+import { View, ViewInput } from "../libs/types/view";
 import ViewModel from "../schema/View.model";
 
 class ViewService {
   private readonly viewModel;
+
   constructor() {
     this.viewModel = ViewModel;
+  }
+
+  public async checkViewExistence(input: ViewInput): Promise<View> {
+    return this.viewModel
+      .findOne({ memberId: input.memberId, viewRefId: input.viewRefId })
+      .exec();
+  }
+  public async insertMemberView(input: ViewInput): Promise<View> {
+    try {
+      return this.viewModel.create(input);
+    } catch (err) {
+      console.log("ERROR, model:insertMemberView:", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATED_FAILED);
+    }
   }
 }
 
